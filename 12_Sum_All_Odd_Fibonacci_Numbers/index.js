2e88c7d24ea95b25244b1190d1ed25dabde70c05
unknown <25971310+MrBanh@users.noreply.github.com>
2019-06-14 14:23:13 -0700

    Problem # 12 Sum All Fibonacci Numbers completed

--- 12_Sum_All_Odd_Fibonacci_Numbers/index.js
+++ 12_Sum_All_Odd_Fibonacci_Numbers/index.js
@@ -1,13 +1,27 @@
+/**
+ * @param {number} num      Positive integer which is used to capture odd Fibonacci numbers <= num
+ * @returns                 Sum of odd fibonacci numbers that are <= num
+ */
 function sumFibs(num) {
-    // Set the base case
-    let f0 = 0;
-    let f1 = 1;
-    let f2 = 1;
+    // Array to capture all fibonacci numbers up to num (argument)
+    const fibs = [1, 1];
 
-    // Array to store all odd fib numbers
-    const oddFibs = [];
+    // Use a loop to generate fibonacci numbers, up to num (inclusive)
+    while (true) {
+        // Generate the next fibonacci number
+        let newFibNum = fibs[fibs.length - 1] + fibs[fibs.length - 2];
+        if (newFibNum > num) { // If that new fibonacci number is > num, break out of loop (don't include in fibs array)
+            break;
+        } else { // The new fibonacci number is still <= num, add to fibs Array
+            fibs.push(newFibNum);
+        }
+    }
 
-    // Loop up to the num
+    // Use the Array.prototype.filter() to filter for only the odd numbers in fibs
+    const oddFibs = fibs.filter(val => val % 2 === 1);
+
+    // Use the Array.prototype.reduce() to add the odd fibonacci numbers and return
+    return oddFibs.reduce((a, b) => a + b);
 }
 
 module.exports = sumFibs;
